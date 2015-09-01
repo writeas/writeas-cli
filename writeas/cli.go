80d0f7653289103dcee54fae6b4ed115f74ac37a
Matt Baer <matt@flashstuf.com>
2015-09-01 10:51:07 -0400

    Improve subcommand help template
    Shows 'writeas' instead of generic 'command'

--- writeas/cli.go
+++ writeas/cli.go
@@ -171,6 +171,20 @@ func main() {
 		},
 	}
 
+	cli.CommandHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   writeas {{.Name}}{{if .Flags}} [command options]{{end}} [arguments...]{{if .Description}}
+
+DESCRIPTION:
+   {{.Description}}{{end}}{{if .Flags}}
+
+OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}{{ end }}
+`
+
 	app.Run(os.Args)
 }
 
