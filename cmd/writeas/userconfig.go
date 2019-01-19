5201295109d0ec6452b1044f01dae1b6708e79dc
a-mt <amistri@live.fr>
2019-01-19 13:11:21 +0100

    Add writeas config command

--- cmd/writeas/userconfig.go
+++ cmd/writeas/userconfig.go
@@ -7,6 +7,10 @@ import (
 	"gopkg.in/ini.v1"
 	"io/ioutil"
 	"path/filepath"
+	"reflect"
+	"fmt"
+	"os"
+	"strings"
 )
 
 const (
@@ -85,6 +89,100 @@ func saveUser(u *writeas.AuthUser) error {
 	err = ioutil.WriteFile(filepath.Join(userDataDir(), userFile), userJSON, 0600)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+// Prints all values of the given struct
+// For subfields: the field name is separated with dots (ex: Posts.Directory=)
+func printConfig(x interface{}, prefix string, showEmptyValues bool) {
+	values := reflect.ValueOf(x)
+
+	if values.Kind() == reflect.Ptr {
+		values = values.Elem()
+	}
+	typ := values.Type()
+
+	for i := 0; i < typ.NumField(); i++ {
+		val  := values.Field(i)
+		name := typ.Field(i).Name
+
+		if prefix != "" {
+			name = prefix + "." + name
+		}
+		if(val.Kind() == reflect.Struct) {
+			printConfig(val.Interface(), name, showEmptyValues)
+		} else {
+			if showEmptyValues || val.Interface() != reflect.Zero(val.Type()).Interface() {
+				fmt.Printf("%s=%v\n", name, val)
+			}
+		}
+	}
+}
+
+// Get the value of a given field
+// For subfields: the name should be separated with dots (ex: Posts.Directory)
+func getConfigField(x interface{}, name string) (*reflect.Value, error) {
+	path   := strings.Split(name, ".")
+	values := reflect.ValueOf(x)
+
+	if values.Kind() == reflect.Ptr {
+		values = values.Elem()
+	}
+	for _, part := range path {
+		values = values.FieldByName(part)
+
+		if !values.IsValid() {
+			err := fmt.Errorf("error: key does not contain a section: %v", name)
+			return nil, err
+		}
+	}
+	if values.Kind() == reflect.Struct {
+		err := fmt.Errorf("error: key does not contain a section: %v", name)
+		return nil, err
+	}
+	return &values, nil
+}
+
+// Opens an editor to modify the config file
+func composeConfig() error {
+	filename := filepath.Join(userDataDir(), userConfigFile)
+
+	// Open the editor
+	cmd := editPostCmd(filename)
+	if cmd == nil {
+		fmt.Println(noEditorErr)
+		return nil
+	}
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	if err := cmd.Start(); err != nil {
+		if debug {
+			panic(err)
+		} else {
+			Errorln("Error starting editor: %s", err)
+			return nil
+		}
+	}
+
+	// Wait until the editor is closed
+	if err := cmd.Wait(); err != nil {
+		if debug {
+			panic(err)
+		} else {
+			Errorln("Editor finished with error: %s", err)
+			return nil
+		}
+	}
+
+	// Check if the config file is valid
+	_, err := loadConfig()
+	if err != nil {
+		if debug {
+			panic(err)
+		} else {
+			Errorln("Error loading config: %s", err)
+			return nil
+		}
 	}
 	return nil
 }
