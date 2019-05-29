6f4570c7bf597db68abae95d160fb90d2ba3b6b1
Rob Loranger <dev@loranger.xyz>
2019-05-29 13:55:44 -0700

    rename cmd writeas list >> writeas posts

--- cmd/writeas/main.go
+++ cmd/writeas/main.go
@@ -152,9 +152,10 @@ func main() {
 			Action: commands.CmdAdd,
 		},
 		{
-			Name:   "list",
-			Usage:  "List local posts",
-			Action: commands.CmdList,
+			Name:        "posts",
+			Usage:       "List posts",
+			Description: `List all local posts.`,
+			Action:      commands.CmdList,
 			Flags: []cli.Flag{
 				cli.BoolFlag{
 					Name:  "id",
