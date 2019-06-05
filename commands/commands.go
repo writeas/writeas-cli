dbe15329af30f2717bce7b917c7411d0c139e6ce
Rob Loranger <dev@loranger.xyz>
2019-06-05 11:05:37 -0700

    return errors on delete command
    changes command for delete to return errors to the user
    
    also updates comment for DoDelete

--- commands/commands.go
+++ commands/commands.go
@@ -104,7 +104,7 @@ func CmdDelete(c *cli.Context) error {
 
 	err := api.DoDelete(c, friendlyID, token, tor)
 	if err != nil {
-		return err
+		log.ErrorlnQuit("Couldn't delete remote copy: %v", err)
 	}
 
 	// Delete local file, if necessary
@@ -113,7 +113,7 @@ func CmdDelete(c *cli.Context) error {
 		// TODO: handle deleting blog posts
 		err = fileutils.DeleteFile(filepath.Join(cfg.Posts.Directory, friendlyID+api.PostFileExt))
 		if err != nil {
-			return err
+			log.ErrorlnQuit("Couldn't delete local copy: %v", err)
 		}
 	}
 
