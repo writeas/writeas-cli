35292d25f547c82faac28bc08e13499616406c08
Rob Loranger <dev@loranger.xyz>
2019-06-07 14:27:55 -0700

    remove unused global flags

--- cmd/wf/flags.go
+++ cmd/wf/flags.go
@@ -14,15 +14,6 @@ var globalFlags = []cli.Flag{
 		Usage: "Use a different port to connect to Tor",
 		Value: 9150,
 	},
-	cli.BoolFlag{
-		Name:  "verbose, v",
-		Usage: "Make the operation more talkative",
-	},
-	cli.StringFlag{
-		Name:  "user-agent",
-		Usage: "Sets the User-Agent for API requests",
-		Value: "",
-	},
 	cli.StringFlag{
 		Name:  "host, H",
 		Usage: "Operate against a custom hostname",
