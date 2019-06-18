8033c564f22f0273cd64d26e59cc6d74cecc6c3c
Rob j Loranger <dev@loranger.xyz>
2019-06-18 14:02:54 -0700

    Revert "add collection listing"
    This reverts commit a8e5ebf719e11218a48ceaf2dc709fb5065dd483.

--- api/collections.go
+++
@@ -1,9 +0,0 @@
-package api
-
-// RemoteColl represents a collection of posts
-// It is a reduced set of data from a go-writeas Collection
-type RemoteColl struct {
-	Alias string
-	Title string
-	URL   string
-}
--- cmd/writeas/main.go
+++ cmd/writeas/main.go
@@ -170,16 +170,6 @@ func main() {
 					Usage: "Show list with URLs",
 				},
 			},
-		}, {
-			Name:   "colls",
-			Usage:  "List collections",
-			Action: commands.CmdCollections,
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "url",
-					Usage: "Show list with URLs",
-				},
-			},
 		},
 		{
 			Name:   "fetch",
