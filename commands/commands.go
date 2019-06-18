85ec32a12443196d47580657f5f631bfa7f9735e
Rob j Loranger <dev@loranger.xyz>
2019-06-18 14:02:54 -0700

    Revert "only show local posts header if there are any"
    This reverts commit 14581da57792403b1c87a5f2d9e52a1a1b116591.

--- commands/commands.go
+++ commands/commands.go
@@ -191,16 +191,14 @@ func CmdListPosts(c *cli.Context) error {
 	var p api.Post
 	posts := api.GetPosts(c)
 	tw := tabwriter.NewWriter(os.Stderr, 10, 0, 2, ' ', tabwriter.TabIndent)
-	numPosts := len(*posts)
-	if ids || !urls && numPosts != 0 {
+	// TODO add no posts found text
+	if ids || !urls {
 		fmt.Fprintf(tw, "Location\t%s\t%s\t\n", "ID", "Token")
-	} else if numPosts != 0 {
-		fmt.Fprintf(tw, "Location\t%s\t%s\t\n", "URL", "Token")
 	} else {
-		fmt.Fprintf(tw, "No local posts found\n")
+		fmt.Fprintf(tw, "Location\t%s\t%s\t\n", "URL", "Token")
 	}
 	for i := range *posts {
-		p = (*posts)[numPosts-1-i]
+		p = (*posts)[len(*posts)-1-i]
 		if ids || !urls {
 			fmt.Fprintf(tw, "local\t%s\t%s\t\n", p.ID, p.EditToken)
 		} else {
